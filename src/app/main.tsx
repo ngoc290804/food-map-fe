7134bdc1647f0ec84ac041e1e584729758a03fc0
Ngoc Nguyen <angoc29082004@gmail.com>
2026-06-20 02:42:08 +0700

    fix vercel

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@/app/App'
 import AppProvider from '@/app/providers/AppProvider'
+import '@/config/leaflet'
 import '@/styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
